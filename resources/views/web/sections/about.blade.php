8228c0bae798b20380127f8addcf7baee5f73e47
Saydum <saydum.it5@gmail.com>
2024-05-24 07:22:13 +0300

    docs(about): добавил текст о компании

--- resources/views/web/sections/about.blade.php
+++ resources/views/web/sections/about.blade.php
@@ -5,15 +5,38 @@
                 <img class="img-fluid rounded" src="{{ asset('web/img/about.jpg') }}">
             </div>
             <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
-                <p class="d-inline-block border rounded text-primary fw-semi-bold py-1 px-3">О компании</p>
-                <h1 class="display-5 mb-4">We Help Our Clients To Grow Their Business</h1>
-                <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
+                <p class="d-inline-block border rounded text-primary fw-semi-bold py-1 px-3">О компании Atomy</p>
+                <h1 class="display-5 mb-4">
+                    Философия продукции ATOMY — абсолютное качество по абсолютной цене.
+                </h1>
+                <p class="mb-4">
+                    <span class="fw-bold">ATOMY</span> - компания родом из Южной Кореи. Основана в 2009 г. и успешно работающая на мировом рынке уже
+                    15 лет, с 2018 г. представлена и в России. Продукция ATOMY изготавливается на фармацевтическом
+                    заводе Kolmar BNH, который также выпускает продукцию для именитых брендов, таких как Lancome,
+                    Сhanel, Shiseido, Nu Skin, Amway, Missha и др.
+
+                </p>
                 <div class="border rounded p-4">
 
                     <div class="tab-content" id="nav-tabContent">
-                        <div class="tab-pane fade show active" id="nav-story" role="tabpanel" aria-labelledby="nav-story-tab">
-                            <p>Tempor erat elitr rebum at clita. Diam dolor diam ipsum et tempor sit. Aliqu diam amet diam et eos labore.</p>
-                            <p class="mb-0">Diam dolor diam ipsum et tempor sit. Aliqu diam amet diam et eos labore. Clita erat ipsum et lorem et sit</p>
+                        <div class="tab-pane fade show active" id="nav-story" role="tabpanel"
+                             aria-labelledby="nav-story-tab">
+                            <p>
+                                ATOMY в индустрии красоты занимает лидирующие позиции, а качество продукции - премиум
+                                класс.
+                            </p>
+                            <p>
+                                Благодаря собственным исследовательским центрам и лаборатории, у ATOMY широкий
+                                ассортимент.
+                            </p>
+                            <p>
+                                Условия хранения и транспортировки на высшем уровне, а продукты защищены от подделки.
+                            </p>
+
+                            <p class="mb-0">
+                                Абсолютная цена — это самая низкая цена, которую может предложить компания без ущерба
+                                для качества товара. Именно эта ценность является самой важной для компании ATOMY.
+                            </p>
                         </div>
                     </div>
                 </div>
--- resources/views/web/sections/facts.blade.php
+++ resources/views/web/sections/facts.blade.php
@@ -1,28 +1,22 @@
 <div class="container-fluid facts my-5 py-5">
     <div class="container py-5">
-        <div class="row g-5">
+        <div class="row g-5 justify-content-center">
             <div class="col-sm-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.1s">
-                <i class="fa fa-users fa-3x text-white mb-3"></i>
-                <h1 class="display-4 text-white" data-toggle="counter-up">1234</h1>
-                <span class="fs-5 text-white">Happy Clients</span>
+                <i class="bi bi-globe  fa-3x text-white mb-3"></i>
+                <h1 class="display-4 text-white" data-toggle="counter-up">24</h1>
+                <span class="fs-5 text-white">В странах</span>
                 <hr class="bg-white w-25 mx-auto mb-0">
             </div>
             <div class="col-sm-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.3s">
-                <i class="fa fa-check fa-3x text-white mb-3"></i>
-                <h1 class="display-4 text-white" data-toggle="counter-up">1234</h1>
-                <span class="fs-5 text-white">Projects Completed</span>
+                <i class="fa fa-users fa-3x text-white mb-3"></i>
+                <h1 class="display-4 text-white" data-toggle="counter-up">16000000</h1>
+                <span class="fs-5 text-white">Партнеров</span>
                 <hr class="bg-white w-25 mx-auto mb-0">
             </div>
             <div class="col-sm-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.5s">
-                <i class="fa fa-users-cog fa-3x text-white mb-3"></i>
-                <h1 class="display-4 text-white" data-toggle="counter-up">1234</h1>
-                <span class="fs-5 text-white">Dedicated Staff</span>
-                <hr class="bg-white w-25 mx-auto mb-0">
-            </div>
-            <div class="col-sm-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.7s">
-                <i class="fa fa-award fa-3x text-white mb-3"></i>
-                <h1 class="display-4 text-white" data-toggle="counter-up">1234</h1>
-                <span class="fs-5 text-white">Awards Achieved</span>
+                <i class="bi bi-bag-check-fill fa-3x text-white mb-3"></i>
+                <h1 class="display-4 text-white" data-toggle="counter-up">700</h1>
+                <span class="fs-5 text-white">Товары</span>
                 <hr class="bg-white w-25 mx-auto mb-0">
             </div>
         </div>
