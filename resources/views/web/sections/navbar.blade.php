2e92c813d209eef480bbb9fda3c5fc97f2347705
Saydum <saydum.it5@gmail.com>
2024-06-04 00:29:51 +0300

    refactor: вывел соц-иконки в отдельный файл

--- resources/views/web/sections/navbar.blade.php
+++ resources/views/web/sections/navbar.blade.php
@@ -25,17 +25,9 @@
                 <a href="/#features" class="nav-item nav-link">Зарабатывать с Atomy</a>
                 <a href="/#footer" class="nav-item nav-link">Контакты</a>
             </div>
-            <div class="d-none d-lg-flex ms-2">
-                <a class="btn btn-light btn-sm-square rounded-circle ms-3" href="">
-                    <small class="fab fa-facebook-f text-primary"></small>
-                </a>
-                <a class="btn btn-light btn-sm-square rounded-circle ms-3" href="">
-                    <small class="fab fa-twitter text-primary"></small>
-                </a>
-                <a class="btn btn-light btn-sm-square rounded-circle ms-3" href="">
-                    <small class="fab fa-linkedin-in text-primary"></small>
-                </a>
-            </div>
+
+{{--            @include('web.sections.social')--}}
+
         </div>
     </nav>
     <!-- alert success -->
---
+++ resources/views/web/sections/social.blade.php
@@ -0,0 +1,11 @@
+<div class="d-none d-lg-flex ms-2">
+    <a class="btn btn-light btn-sm-square rounded-circle ms-3" href="">
+        <small class="fab fa-facebook-f text-primary"></small>
+    </a>
+    <a class="btn btn-light btn-sm-square rounded-circle ms-3" href="">
+        <small class="fab fa-twitter text-primary"></small>
+    </a>
+    <a class="btn btn-light btn-sm-square rounded-circle ms-3" href="">
+        <small class="fab fa-linkedin-in text-primary"></small>
+    </a>
+</div>
