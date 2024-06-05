30c41fb39fdf71c4f0d740743da2acdcbe797690
Saydum <saydum.it5@gmail.com>
2024-06-05 04:34:07 +0300

    chore: :pencil2: подправил текст

--- resources/views/web/sections/facts.blade.php
+++ resources/views/web/sections/facts.blade.php
@@ -3,7 +3,7 @@
         <div class="row g-5 justify-content-center">
             <div class="col-sm-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.1s">
                 <i class="bi bi-globe  fa-3x text-white mb-3"></i>
-                <h1 class="display-4 text-white" data-toggle="counter-up">24</h1>
+                <h1 class="display-4 text-white" data-toggle="counter-up">26</h1>
                 <span class="fs-5 text-white">В странах</span>
                 <hr class="bg-white w-25 mx-auto mb-0">
             </div>
@@ -16,7 +16,7 @@
             <div class="col-sm-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.5s">
                 <i class="bi bi-bag-check-fill fa-3x text-white mb-3"></i>
                 <h1 class="display-4 text-white" data-toggle="counter-up">700</h1>
-                <span class="fs-5 text-white">Товары</span>
+                <span class="fs-5 text-white">Товаров премиум класса</span>
                 <hr class="bg-white w-25 mx-auto mb-0">
             </div>
         </div>
